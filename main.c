cec52e6279dddd069d2434338a4c07d5c8a50345
MatheusCervieri <mathcervieri@gmail.com>
2022-07-18 18:43:39 +0200

    Execve undestood and fork understood

--- main.c
+++ main.c
@@ -12,12 +12,19 @@ int main ()
 //This way we can have a child process that will run the progran that we open with exect function and 
 //after that we are coming back to the parent process. 
 
+
+char *cmd = "/usr/bin/ls";  //PATH of the comand! 
+char *argVec[] = {"ls", "-a" , NULL};
+char *envVec[] = {NULL};
+
 int pid = fork(); //process id = fork. 
-if (pid == -1)
+if (pid == -1) // since it replaces the process value, the return value is always -1. 
 	return (1); 
 
-if (pid == 0)
+if (pid == 0) 
 {
+	if(execve(cmd, argVec, envVec) == -1) 
+		perror("Culd not execute execve"); 
 	printf("This is the child process \n");
 	//Child process. 
 }
