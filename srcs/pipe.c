7ea51a110ee2db37a059292c3422de19b0e4c380
MatheusCervieri <mathcervieri@gmail.com>
2022-07-19 17:58:27 +0200

    Pipe working

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -9,10 +9,21 @@ void open_cmd()
 		exit_program("Couldn't open the program");
 }
 
+void open_grep()
+{
+	char *cmd = "/usr/bin/grep";  
+	char *argVec[] = {"grep", "a" , NULL};
+	char *envVec[] = {NULL};
+	if(execve(cmd, argVec, envVec) == -1) 
+		exit_program("Couldn't open the program");
+}
+
+
 void pipe_operator()
 {
 	int fd[2];
 	int pid1;
+	int pid2;
 
 	if(pipe(fd) == -1)
 		exit_program("Pipe function error");
@@ -27,5 +38,18 @@ void pipe_operator()
 		open_cmd();
 		//child process. 
 	}
+	pid2 = fork();
+	if(pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		open_grep();
+		//child process. 
+	}
+
+	close(fd[0]);
+	close(fd[1]);
 	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 }
